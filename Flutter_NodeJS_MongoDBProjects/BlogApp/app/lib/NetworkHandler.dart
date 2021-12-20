188007117b05d8c86f1d44edef286cf35247fd39
Harshit Rathi <harshitr2001@gmail.com>
2021-12-20 14:03:38 +0530

    updates

--- Flutter_NodeJS_MongoDBProjects/BlogApp/app/lib/NetworkHandler.dart
+++ Flutter_NodeJS_MongoDBProjects/BlogApp/app/lib/NetworkHandler.dart
@@ -82,23 +82,7 @@ class NetworkHandler {
     return jsonDecode(response.body);
   }
 
-  Future<http.StreamedResponse> patchImage(String url, String filepath) async {
-    url = formater(url);
-    String token = await storage.read(key: "token");
-    var request = http.MultipartRequest('PATCH', Uri.parse(url));
-    request.files.add(await http.MultipartFile.fromPath("img", filepath));
-    request.headers.addAll(
-        {"Content-type": "multipart/form-data", "Authorization": token});
-    var response = request.send();
-    return response;
-  }
-
   String formater(String url) {
     return baseurl + url;
-  }
-
-  NetworkImage getImage(String imageName) {
-    String url = formater("/uploads//$imageName.jpg");
-    return NetworkImage(url);
   }
 }
